e75b58663528294d31c0314238bd4972ca24b37a
Sujal0041 <152766716+Sujal0041@users.noreply.github.com>
2024-01-22 21:12:39 +0545

    COmponent importing

---
+++ src/components/todocontainer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import  Todoform  from './todoform'
+const todocontainer = () => {
+  return (
+    <div className='todocontainer'>
+      <Todoform />
+    </div>
+  )
+}
+
+export default todocontainer
