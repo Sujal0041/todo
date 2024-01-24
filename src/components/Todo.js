c0867d0c3535f5d781f0720cd8aedfb271a33d2e
Sujal0041 <152766716+Sujal0041@users.noreply.github.com>
2024-01-24 15:52:51 +0545

    CSS Implementation

---
+++ src/components/Todo.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Todo = () => {
+  return (
+    <div className='Todo'>
+    <p>Go To College</p>      
+    </div>
+  )
+}
+
+export default Todo
--- src/components/TodoMain.js
+++ src/components/TodoMain.js
@@ -1,17 +1,19 @@
 import React, { useState} from 'react'
 import  TodoformData  from './TodoFormData'
 import { v4 as uuidv4 } from 'uuid';
+import Todo from './Todo';
 uuidv4()
 const TodoMain = () => {
     const [todos,setTodos] = useState([])
 
     const addTodos = (todo) =>{
-        setTodos([...todos,{id: uuidv4(),task:todo,completed:false, isEditing: false}])
+        setTodos([...todos,{id: uuidv4(),task:todo,completed:false}])
         console.log(todos)
     }
   return (
     <div className='todocontainer'>
       <TodoformData addTodo={addTodos}/>
+      <Todo />
     </div>
   )
 }
