e2756bf74f2fc6fce3b06d3f5aa9d978994e0275
Sujal0041 <152766716+Sujal0041@users.noreply.github.com>
2024-01-24 15:39:24 +0545

    Used Props, Store Target Value, Clear After clicking Add Button, Submission Behavior

--- src/components/TodoFormData.js
+++ src/components/TodoFormData.js
@@ -1,10 +1,23 @@
 import React, { useState } from 'react'
 
-const TodoFormData = () => {
+const TodoFormData = ({addTodo}) => {
   const [value,setValue] = useState("")
+
+  const handleInputChange = (e) => {
+    // Update the 'value' with the current value of the input field
+    setValue(e.target.value);
+  };
+
+  const handlesubmit = (e) => {
+    // Prevent the default form submission behavior
+    e.preventDefault();
+    addTodo(value)
+    setValue("") // After clicking the button, clear the input field
+  };
+
   return (
-    <form className='todoform'>
-        <input type='text' className='todoinput' placeholder='Enter Todo' onChange={(e) => console.log(e.target.value)}/>
+    <form className='todoform' onSubmit={handlesubmit}>
+        <input type='text' className='todoinput'value={value} placeholder='Enter Todo'  onChange={handleInputChange}/>
         <button type='submit' className='addtodo'>Add Todo</button>
     </form>
   )
--- src/components/TodoMain.js
+++ src/components/TodoMain.js
@@ -1,9 +1,17 @@
-import React from 'react'
+import React, { useState} from 'react'
 import  TodoformData  from './TodoFormData'
+import { v4 as uuidv4 } from 'uuid';
+uuidv4()
 const TodoMain = () => {
+    const [todos,setTodos] = useState([])
+
+    const addTodos = (todo) =>{
+        setTodos([...todos,{id: uuidv4(),task:todo,completed:false, isEditing: false}])
+        console.log(todos)
+    }
   return (
     <div className='todocontainer'>
-      <TodoformData />
+      <TodoformData addTodo={addTodos}/>
     </div>
   )
 }
